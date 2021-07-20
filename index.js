75a56fca89e3dda2205f7e0cd04051feebb017b6
Lê Văn Thiện <52475801+epitchi@users.noreply.github.com>
2021-07-20 15:46:34 +0700

    add try catch to cover wrong message

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const wss = new WebSocket.Server({ port: 8082 });
 
 wss.on("connection", ws => {
     ws.on("message", message => {
-        //back into native JavaScript
-        const data =JSON.parse(message);
-        console.log(data.x, data.y)
-        
+        try {
+            //back into native JavaScript
+            const data = JSON.parse(message);
+
+            console.log(data.x, data.y)
+        } catch (e) {
+            console.log(`Something went wrong with the message: ${e.message}`);
+        }
+
     });
 });
