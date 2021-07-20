b32748473a6aee0a43456ddf944e914221ec4f6b
Lê Văn Thiện <52475801+epitchi@users.noreply.github.com>
2021-07-20 15:37:34 +0700

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8082 });
+
+wss.on("connection", ws => {
+    ws.on("message", message => {
+        console.log(message);
+    });
+});
